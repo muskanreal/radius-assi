9ea0da44e53466103800409b06c0e5404da52bd6
muskanreal <muskanmalik01062006@gmail.com>
2025-04-26 04:23:00 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let r = parseFloat(prompt("enter the radius of circle "));
+
+let diameter = 2*r;
+let circumFerence = 2*3.14*r;
+let area = 3.14*(r**2);
+
+console.log(`Diameter is: ${diameter}`);
+console.log(`Circumference is: ${circumFerence}`);
+console.log(`Area is: ${area}`);
+
+
